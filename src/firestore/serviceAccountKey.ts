f37f05f7fdb2a230a5604c40c0c08e0d020bc6ba
dev-ptera <dev.ptera@gmail.com>
2021-05-02 14:24:26 -0400

    Fix local environment

--- src/firestore/serviceAccountKey.ts
+++ src/firestore/serviceAccountKey.ts
@@ -1,8 +1,10 @@
+const private_value = process.env.PRIVATE_KEY.replace(/\\n/g, '\n');
+
 export const serviceAccount = {
   "type": "service_account",
   "project_id": "jungle-pixel-art",
-  "private_key_id": process.env.private_key_id,
-  "private_key": process.env.private_key,
+  "private_key_id": "71a0de1da5513d3ff4709d06af0974c08e15767b",
+  "private_key": private_value,
   "client_email": "firebase-adminsdk-ya9ou@jungle-pixel-art.iam.gserviceaccount.com",
   "client_id": "116762069232062032742",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
